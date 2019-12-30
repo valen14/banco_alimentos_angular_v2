7413e54c953d4d56ac27cfc395ce8821934c51c8
Federico Di Claudio <federicodicla@gmail.com>
2019-12-30 17:58:19 -0300

    Se borraron componentes no utilizados

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -4,12 +4,6 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
 import { UsersComponent } from './admin/users/users.component';
-import { BeneficiariosComponent } from './admin/users/beneficiarios/beneficiarios.component';
-import { DonantesComponent } from './admin/users/donantes/donantes.component';
-import { VoluntariosComponent } from './admin/users/voluntarios/voluntarios.component';
-import { VoluntarioComponent } from './admin/users/voluntarios/voluntario/voluntario.component';
-import { DonanteComponent } from './admin/users/donantes/donante/donante.component';
-import { BeneficiarioComponent } from './admin/users/beneficiarios/beneficiario/beneficiario.component';
 import { BultosComponent } from './admin/bultos/bultos.component';
 import { BultoComponent } from './admin/bultos/bulto/bulto.component';
 import { EnviosComponent } from './admin/envios/envios.component';
@@ -26,7 +20,7 @@ import { NuevoEnvioComponent } from './admin/envios/nuevo-envio/nuevo-envio.comp
 
 
 @NgModule({
-  declarations: [AdminComponent,  UsersComponent, BeneficiariosComponent, DonantesComponent, VoluntariosComponent, VoluntarioComponent, DonanteComponent, BeneficiarioComponent, BultosComponent, BultoComponent, EnviosComponent, EnvioComponent, CarsComponent, CarComponent, PaquetesComponent, SeleccionPaquetesComponent, SeleccionVoluntarioComponent, SinAsignarComponent, AsignadosComponent,NuevoEnvioComponent],
+  declarations: [AdminComponent,  UsersComponent,  BultosComponent, BultoComponent, EnviosComponent, EnvioComponent, CarsComponent, CarComponent, PaquetesComponent, SeleccionPaquetesComponent, SeleccionVoluntarioComponent, SinAsignarComponent, AsignadosComponent,NuevoEnvioComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
