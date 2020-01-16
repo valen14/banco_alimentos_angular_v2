703b6007d99aa1e6fd30b66e36a36a916e13ffa1
Valentin <vccarp@gmail.com>
2020-01-16 17:08:01 -0300

    trabaje en voluntario

--- src/app/components/voluntario/voluntario-routing.module.ts
+++ src/app/components/voluntario/voluntario-routing.module.ts
@@ -6,44 +6,22 @@ import { EnvioComponent } from './voluntario/envios/envio/envio.component';
 import { CarComponent } from './voluntario/cars/car/car.component';
 import { CarsComponent } from './voluntario/cars/cars.component';
 import { InsignasComponent } from './voluntario/insignas/insignas.component';
-import { AceptadosComponent } from './voluntario/envios/aceptados/aceptados.component';
-import { RechazadosComponent } from './voluntario/envios/rechazados/rechazados.component';
-import { PendientesComponent } from './voluntario/envios/aceptados/pendientes/pendientes.component';
-import { CanceladosComponent } from './voluntario/envios/aceptados/cancelados/cancelados.component';
-import { RealizadosComponent } from './voluntario/envios/aceptados/realizados/realizados.component';
-import { SolicitudesComponent } from './voluntario/envios/solicitudes/solicitudes.component';
 
 
 const routes: Routes = [
-  {path: '', component: VoluntarioComponent,
-  children:[
-    {path: 'envios', component: EnviosComponent,
-      children:[
-        {path: 'aceptados', component: AceptadosComponent,
-          children:[
-            {path: 'pendientes', component: PendientesComponent},
-            {path: 'pendientes/:id', component: EnvioComponent},
-            {path: 'cancelados', component: CanceladosComponent},
-            {path: 'cancelados/:id', component: EnvioComponent},
-            {path: 'realizados', component: RealizadosComponent},
-            {path: 'realizados/:id', component: EnvioComponent}
-          ]
-      },
-        {path: 'rechazados', component: RechazadosComponent},
-        {path: 'rechazados/:id', component: EnvioComponent},
-        {path: 'solicitudes', component: SolicitudesComponent},
-        {path: 'solicitudes/:id', component: EnvioComponent},
-        
-      ]
-      
-      },
-        
-        {path: 'cars', component: CarsComponent},
-        {path: 'cars/:id', component: CarComponent},
-        {path: 'insignas', component: InsignasComponent}
-      ]
-}
+  {
+    path: '', component: VoluntarioComponent,
+    children:[
+      {path: 'envios/:filter', component: EnviosComponent},
+      {path: 'envios/:id/ver', component: EnvioComponent},           
+      {path: 'cars', component: CarsComponent},
+      {path: 'cars/:id', component: CarComponent},
+      {path: 'insignas', component: InsignasComponent}
+    ]
+  }
 ];
+
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/voluntario/voluntario.module.ts
+++ src/app/components/voluntario/voluntario.module.ts
@@ -8,17 +8,11 @@ import { EnvioComponent } from './voluntario/envios/envio/envio.component';
 import { CarsComponent } from './voluntario/cars/cars.component';
 import { CarComponent } from './voluntario/cars/car/car.component';
 import { InsignasComponent } from './voluntario/insignas/insignas.component';
-import { RechazadosComponent } from './voluntario/envios/rechazados/rechazados.component';
-import { AceptadosComponent } from './voluntario/envios/aceptados/aceptados.component';
-import { PendientesComponent } from './voluntario/envios/aceptados/pendientes/pendientes.component';
-import { CanceladosComponent } from './voluntario/envios/aceptados/cancelados/cancelados.component';
-import { RealizadosComponent } from './voluntario/envios/aceptados/realizados/realizados.component';
-import { SolicitudesComponent } from './voluntario/envios/solicitudes/solicitudes.component';
 
 
 
 @NgModule({
-  declarations: [VoluntarioComponent, EnviosComponent, EnvioComponent, CarsComponent, CarComponent, InsignasComponent, RechazadosComponent, AceptadosComponent, PendientesComponent, CanceladosComponent, RealizadosComponent, SolicitudesComponent],
+  declarations: [VoluntarioComponent, EnviosComponent, EnvioComponent, CarsComponent, CarComponent, InsignasComponent, ],
   imports: [
     CommonModule,
     VoluntarioRoutingModule
