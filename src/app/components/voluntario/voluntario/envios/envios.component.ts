d383a68fa0a3f07711a71b1f45218bef84dc5129
Valentin <vccarp@gmail.com>
2020-01-16 18:27:45 -0300

    panel voluntario

--- src/app/components/voluntario/voluntario/envios/envios.component.ts
+++ src/app/components/voluntario/voluntario/envios/envios.component.ts
@@ -8,58 +8,57 @@ import { EnvioApi, VoluntarioApi, Envio } from 'src/app/service/lbservice';
   styleUrls: ['./envios.component.css']
 })
 export class EnviosComponent implements OnInit {
-
+  idUserLog
   filter: String
   envios = []
+  //enviosVisualizados=[]
   voluntarios=[]
 
   constructor(private ar: ActivatedRoute,
     private router: Router,
     private envioService: EnvioApi, private voluntarioService: VoluntarioApi) { 
+      const emailUserLog= sessionStorage.getItem('email')
+      this.voluntarioService.findOne({where:{email: emailUserLog}}).subscribe((voluntario) => {
+        console.log(voluntario)
+        this.idUserLog=voluntario['id']    
+        this.obtenerEnvios()
+      })
+    }
+
+    private obtenerEnvios(){
+      this.filter = this.ar.snapshot.params['filter']
+      console.log(this.filter)
+      this.voluntarioService.getEnvios(this.idUserLog).subscribe((envios) => {
+        console.log(envios),
+        this.envios = envios
+      })
+      switch(this.filter){
+        case 'todos': 
+          //hacer
+          break
+        case 'solicitudes':
+    
+          break
+        case 'aceptados':
+    
+          break
+        case 'denegados':
+    
+          break
+        }
+    
 
     }
 
   ngOnInit() {
-    this.filter = this.ar.snapshot.params['filter']
+    /*this.filter = this.ar.snapshot.params['filter']
     console.log(this.filter)
     this.ar.paramMap.subscribe((params) => {
       //this.filter = params.get('filter')
       this.cargarTabla()
-    })
+    }) */
   }
 
-  private cargarTabla(){
-    switch(this.filter){
-    case 'todos': 
-      this.envioService.find().subscribe((envios)=>{
-        console.log(envios)
-        this.envios=envios
-      })
-      break
-    case 'sin-asignar':
-      this.envioService.find({where:{estado:'pendiente de asignacion'}}).subscribe((envios)=>{
-        console.log(envios)
-        this.envios=envios
-      })
-      break
-    case 'asignados':
-      this.envioService.find({where:{estado:'pendiente de traslado'}}).subscribe((envios)=>{
-        console.log(envios)
-        this.envios=envios
-      })
-      break
-    case 'realizados':
-      this.envioService.find({where:{estado:'realizado'}}).subscribe((envios)=>{
-        console.log(envios)
-        this.envios=envios
-      })
-      break
-    }
-    this.voluntarioService.find().subscribe((voluntarios)=>{
-      console.log(voluntarios)
-      this.voluntarios=voluntarios
-    })
-  }
 
   verEnvioButtonClick(id: any) {
     alert("a desarrollar")
