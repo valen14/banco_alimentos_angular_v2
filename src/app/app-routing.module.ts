6571862a61a72941d91ab8afc20c0f5e374ab1ee
Valentin <vccarp@gmail.com>
2020-02-04 13:38:18 -0300

    sobre nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   //dinámico
   { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
-  { path: 'about', loadChildren: () => import('./components/about/about.module').then(m => m.AboutModule) },
+  { path: 'about/:filter', loadChildren: () => import('./components/about/about.module').then(m => m.AboutModule) },
   { path: 'register', loadChildren: () => import('./components/register/register.module').then(m => m.RegisterModule) },
   { path: 'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule) },
   { path: 'contact', loadChildren: () => import('./components/contact/contact.module').then(m => m.ContactModule) },
